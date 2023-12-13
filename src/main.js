fa1e0b9e1999daa5380e63d5c907035f28def7db
Josué Estrada <dev.josuestrd@gmail.com>
2023-12-13 02:33:34 -0500

    Update fonts and menu styles, add responsive layout

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import "./scss/main.scss";
+
+const toggleMenu = document
+  .getElementsByClassName("header__btn")[0]
+  .addEventListener("click", () => {
+    /* const navList = document.getElementsByClassName("nav__list")[0];
+    navList.style.display =
+      navList.style.display === "none" || navList.style.display === ""
+        ? "block"
+        : "none"; */
+    const navList = document
+      .getElementsByClassName("nav__list")[0]
+      .classList.toggle("show-menu");
+  });
+
+toggleMenu;
